f46ff8545eaaff9e73005486bedfd2a718aad163
ashleshatraining <ashlesha.chittem7@gmail.com>
2025-08-12 22:09:37 +0530

    Remove hardcoded secret from main.tf

---
+++ TF-files/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type = string
+  description = "prefix value"
+
+}
+variable "VMNAME" {
+    type = string
+    description = "vm user name"  
+}
+
+variable "SLOGIN" {
+  type = string
+  description = "sql login"
+}
