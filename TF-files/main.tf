a8a2e011d3b42013ccaba39c3626c2f9d6e7cb5a
ashleshatraining <56999228+ashleshatraining@users.noreply.github.com>
2025-08-13 19:30:58 +0530

    Update main.tf

--- TF-files/main.tf
+++ TF-files/main.tf
@@ -2,9 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      skip_provider_registration = true
       version = "=3.0.0"
     }
+ provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
 
   }
   backend "azurerm" {
