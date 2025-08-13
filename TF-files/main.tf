4d640fc9b364900b79179ebdcec4ceae918fd7a0
ashleshatraining <56999228+ashleshatraining@users.noreply.github.com>
2025-08-13 16:54:11 +0530

    Update main.tf

--- TF-files/main.tf
+++ TF-files/main.tf
@@ -4,25 +4,26 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+
   }
   backend "azurerm" {
-    resource_group_name  = "Paritala"
-    storage_account_name = "casestudy3sa1"
-    container_name       = "container12"
+    resource_group_name  = "backendazurerg"
+    storage_account_name = "backendrg"
+    container_name       = "testbacknd"
     key                  = "terraform.tfstate"
   }  
 }
 provider "azurerm" {
   features {}
-  #subscription_id = "de108057-2edc-44c6-9cdc-365886498d3e"
-  #client_id       = "450e193b-0a68-445d-acaa-a9105d57060b"
-  #tenant_id       = "7c733756-1a90-400f-863b-e0c6877412e8"
-  #client_secret   = "var.secret"
+  subscription_id = "426b8104-3a89-42f0-a3f5-14b554b1b2bb"
+  client_id       = "bf5e8e4f-3aea-40f9-bba6-0978ac70f104"
+  tenant_id       = "09201aea-fd49-442b-9a0d-8de2389cefaf"
+  client_secret   = "cfcc3286-bcc7-4b08-b99b-87289748649a"
 }
 
 
 data "azurerm_resource_group" "rg" {
-  name = "Paritala"
+  name = "backendazurerg"
 }
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}vnetwork"
